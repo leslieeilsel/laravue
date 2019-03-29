186ad563e70949aba5795e73846f7cf250fcb7df
leslieeilsel <1178273431@qq.com>
2019-03-29 11:54:18 +0800

    调整首页

--- resources/views/slogan.blade.php
+++ resources/views/slogan.blade.php
@@ -13,13 +13,22 @@
             height: 100%;
         }
 
+        body {
+            background: #f4f5f7;
+        }
+
         .container {
             display: table;
             height: 100%;
+            overflow-y: visible
         }
 
         .slogan {
-            margin-top: 60px;
+            margin-top: 10px;
+        }
+
+        .slogan span {
+            letter-spacing: 1px;
         }
     </style>
 </head>
@@ -33,7 +42,7 @@
                 </strong>
             </span>
         </p>
-        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 4em;">
+        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 2em;">
             <strong>
                 <span style="font-family: 微软雅黑, Microsoft YaHei; font-size: 20px;">沣西新城的开发、建设、运营服务商</span>
             </strong>
@@ -45,7 +54,7 @@
                 </strong>
             </span>
         </p>
-        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 4em;">
+        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 2em;">
             <strong>
                 <span style="font-size: 20px; font-family: 微软雅黑, Microsoft YaHei;">新型城市开发运营的典范</span>
             </strong>
@@ -57,7 +66,7 @@
                 </strong>
             </span>
         </p>
-        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 4em;">
+        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 2em;">
             <strong>
                 <span style="font-size: 20px; font-family: 微软雅黑, Microsoft YaHei;">担当 奉献 创新 突破</span>
             </strong>
@@ -69,10 +78,16 @@
                 </strong>
             </span>
         </p>
-        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 4em;">
+        <p style="text-indent: 0em; white-space: normal; text-align: center; line-height: 2em;">
             <strong>
                 <span style="font-size: 20px; font-family: 微软雅黑, Microsoft YaHei;">务实求真 创新进取</span>
             </strong>
+        </p>
+    </div>
+    <div class="profile">
+        <img src="http://www.xixianjituan.com/uploadfile/20180312/1520846890943382.jpg" alt="" width="100%">
+        <p>
+            <span style="color: rgb(110, 110, 110); font-size: 14px; text-align: justify; text-indent: 28px; background-color: rgb(244, 245, 247); font-family: arial, helvetica, sans-serif;">&nbsp;&nbsp;&nbsp;&nbsp;陕西省西咸新区沣西新城开发建设（集团）有限公司（简称“沣西集团”）是根据沣西新城管委会《关于设立陕西省西咸新区沣西新城开发建设（集团）有限公司的决定》（沣西管发〔2011〕12 号）批准成立的国有独资公司，于2011 年10 月11 日取得陕西省工商行政管理局核发的《企业法人营业执照》。公司是西咸新区沣西新城最重要的基础设施投资建设主体，目前已经形成基础设施代管、土地开发整理、保障房和市政项目代建等主要业务板块，同时公司积极开拓广告、管廊租赁、房地产开发等业务。</span>
         </p>
     </div>
 </div>
