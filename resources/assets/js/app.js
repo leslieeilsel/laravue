c928cc6329807875388fc65b212ec21745db9c6d
leslieeilsel <1178273431@qq.com>
2019-04-10 12:48:53 +0800

    增加扩展包

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,6 +6,7 @@ import App from './app.vue'
 import store from './store'
 import i18n from './lang'
 import {router} from './router'
+import VueResource from 'vue-resource'
 
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
@@ -15,6 +16,7 @@ import 'ant-design-vue/dist/antd.css'
 
 Vue.use(iView);
 Vue.use(Antd);
+Vue.use(VueResource)
 
 const app = new Vue({
   el: '#app',
