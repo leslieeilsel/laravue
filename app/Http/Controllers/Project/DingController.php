a32ce016b49864b56a8dd133870451174182d044
wmin_f <1208444024@qq.com>
2019-05-21 16:55:41 +0800

    test

--- app/Http/Controllers/Project/DingController.php
+++ app/Http/Controllers/Project/DingController.php
@@ -17,7 +17,7 @@ class DingController extends Controller
         // curl_close($ch);
         // $arr=json_decode($json,1);
         // config(["auth.Ding_Access_Token"=>$arr['access_token']]);
-        dd(config("auth.Ding_Access_Token"));
+        dd(config("auth.passwords.users.provider"));
     }
     
 }
