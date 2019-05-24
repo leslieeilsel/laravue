95a343e10428140d4d6901b5a12c106d9b235afd
wmin_f <1208444024@qq.com>
2019-05-24 15:42:23 +0800

    ding test

--- app/Http/Controllers/Project/DingController.php
+++ app/Http/Controllers/Project/DingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Project;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
+use function GuzzleHttp\json_encode;
+
 class DingController extends Controller
 {
     public function getToken(){
@@ -28,27 +30,27 @@ class DingController extends Controller
     }
     public function userNotify(){
         $appKey=env("Ding_App_Key");
-        // $appSecret=env("Ding_App_Secret");
-        // $accessToken=Cache::get('dingAccessToken');
-        // $signInfo=$this->sign();
-        // $url='https://oapi.dingtalk.com/sns/getuserinfo_bycode';
-        // $ch = curl_init();
-        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        // curl_setopt($ch, CURLOPT_POST, TRUE);
-        // //设置post数据
-        // $post_data = array(
-        //     "accessKey" => $signInfo['appId'],
-        //     "timestamp" => $signInfo['time'],
-        //     "signature"=>$signInfo['sign']
-        //     );
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-        // curl_setopt($ch, CURLOPT_URL, $url);
-        // $json =  curl_exec($ch);
-        // curl_close($ch);
+        $appSecret=env("Ding_App_Secret");
+        $accessToken=Cache::get('dingAccessToken');
+        $signInfo=$this->sign();
+        $url='https://oapi.dingtalk.com/sns/getuserinfo_bycode';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        //设置post数据
+        $post_data = array(
+            "accessKey" => $signInfo['appId'],
+            "timestamp" => $signInfo['time'],
+            "signature"=>$signInfo['sign']
+            );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $json =  curl_exec($ch);
+        curl_close($ch);
         // $arr=json_decode($json,true);
         // dd($arr);
-        return $appKey;
+        return json_encode($post_data);
     }
     public function sign(){
         $appSecret=env("Ding_App_Secret");
