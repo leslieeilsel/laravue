e7623552b6bb0e4e0bd9cc1050b6ec495108a28b
wmin_f <1208444024@qq.com>
2019-05-27 15:19:00 +0800

    ding test

--- app/Http/Controllers/Project/DingController.php
+++ app/Http/Controllers/Project/DingController.php
@@ -46,7 +46,7 @@ class DingController extends Controller
         $user_id_url='https://oapi.dingtalk.com/user/getuserinfo?access_token='.$accessToken.'&code='.$data['code'];
         $user_ids=$this->postCurl($user_id_url,[],'get');
         $user_id=json_decode($user_ids,true);
-        $url='https://oapi.dingtalk.com/user/get?access_token='.$accessToken.'&userid'.$user_id['userid'];
+        $url='https://oapi.dingtalk.com/user/get?access_token='.$accessToken.'&userid='.$user_id['userid'];
         $json=$this->postCurl($url,[],'get');
         return $json;
     }
