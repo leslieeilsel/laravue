b4fd96f421d314f735205b7d8b5a59051759e9c4
wmin_f <1208444024@qq.com>
2019-05-27 17:03:42 +0800

    ding test

--- resources/assets/js/views/ding/project/index.vue
+++ resources/assets/js/views/ding/project/index.vue
@@ -185,7 +185,7 @@ import * as dd from 'dingtalk-jsapi'
 						corpId: "dinge48f324dae7de1df35c2f4657eb6378f",
 						onSuccess: function(result) {
 							getUserId({code:result.code}).then(res => {
-								alert(JSON.stringify(res));
+								alert(JSON.stringify(res)+"$$$");
 								if(res.errcode==0){									
 								}
 							});
