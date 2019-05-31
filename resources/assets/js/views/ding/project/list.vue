d069cb632f8779d5d153c07824db3669e981e64b
wmin_f <1208444024@qq.com>
2019-05-31 12:51:03 +0800

    ding test

--- resources/assets/js/views/ding/project/list.vue
+++ resources/assets/js/views/ding/project/list.vue
@@ -1,43 +1,45 @@
 <template>
     <div class="mui-content"> 
-			<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
-        {{con_str}}
-				<!-- <li class="mui-table-view-cell">
-					<div class="mui-table">
-						<div class="mui-table-cell mui-col-xs-10">
-							<h4 class="mui-ellipsis">信息化推进办公室张彦合同付款信息化</h4>
-							<h5>申请人：李四</h5>
-							<p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
-						</div>
-						<div class="mui-table-cell mui-col-xs-2 mui-text-right">
-							<span class="mui-h5">12:25</span>
-						</div>
-					</div>
-				</li>
-				<li class="mui-table-view-cell">
-					<div class="mui-table">
-						<div class="mui-table-cell mui-col-xs-10">
-							<h4 class="mui-ellipsis-2">信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款</h4>
-							<h5>申请人：李四</h5>
-							<p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
-						</div>
-						<div class="mui-table-cell mui-col-xs-2 mui-text-right">
-							<span class="mui-h5">12:25</span>
-						</div>
-					</div>
-				</li> -->
-			</ul>
+      <div class="mui-content"> 
+        <ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
+          {{con_str}}
+          <!-- <li class="mui-table-view-cell">
+            <div class="mui-table">
+              <div class="mui-table-cell mui-col-xs-10">
+                <h4 class="mui-ellipsis">信息化推进办公室张彦合同付款信息化</h4>
+                <h5>申请人：李四</h5>
+                <p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
+              </div>
+              <div class="mui-table-cell mui-col-xs-2 mui-text-right">
+                <span class="mui-h5">12:25</span>
+              </div>
+            </div>
+          </li>
+          <li class="mui-table-view-cell">
+            <div class="mui-table">
+              <div class="mui-table-cell mui-col-xs-10">
+                <h4 class="mui-ellipsis-2">信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款</h4>
+                <h5>申请人：李四</h5>
+                <p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
+              </div>
+              <div class="mui-table-cell mui-col-xs-2 mui-text-right">
+                <span class="mui-h5">12:25</span>
+              </div>
+            </div>
+          </li> -->
+        </ul>
+      </div>
+      <nav class="mui-bar mui-bar-tab">
+        <a class="mui-tab-item mui-active" href="/ding/project">
+          <span class="mui-icon mui-icon-home"></span>
+          <span class="mui-tab-label">应用</span>
+        </a>
+        <a class="mui-tab-item" href="#">
+          <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+          <span class="mui-tab-label">消息</span>
+        </a>
+      </nav>
     </div>
-    <nav class="mui-bar mui-bar-tab">
-      <a class="mui-tab-item mui-active" href="/ding/project">
-        <span class="mui-icon mui-icon-home"></span>
-        <span class="mui-tab-label">应用</span>
-      </a>
-      <a class="mui-tab-item" href="#">
-        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-        <span class="mui-tab-label">消息</span>
-      </a>
-    </nav>
 </template>
 <style scope src="./index.css"></style>
 <style scope src="./mui.css"></style>
@@ -64,6 +66,7 @@ export default {
         return false;
       }
       getAllProjects({userid:sessionStorage.getItem('userid')}).then(e => {
+          alert(JSON.stringify(e))
           if(e.result){
             let str='';
             e.result.forEach(function (row, index) {
