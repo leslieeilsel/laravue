ca741539000fa7bd14c197002a4b6f529a4e7c2c
wmin_f <1208444024@qq.com>
2019-05-31 11:43:12 +0800

    ding test

--- resources/assets/js/views/ding/project/list.vue
+++ resources/assets/js/views/ding/project/list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mui-content">
     <div class="mui-content"> 
-			<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
+			<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed" id='ul_content'>
 				<li class="mui-table-view-cell">
 					<div class="mui-table">
 						<div class="mui-table-cell mui-col-xs-10">
@@ -60,36 +60,33 @@ export default {
     },
     getProject() {
       if(sessionStorage.getItem('userid')==''){
-        this.$Message.success("请重新获取用户信息");
+        this.$Message.error("请重新获取用户信息");
         return false;
       }
-      alert(sessionStorage.getItem('userid'))
       getAllProjects({userid:sessionStorage.getItem('userid')}).then(e => {
           alert(JSON.stringify(e))
+          if(e.result){
+            let str='';
+            e.result.forEach(function (row, index) {
+              str += '<li class="mui-table-view-cell">'+
+                    '<div class="mui-table">'+
+                      '<div class="mui-table-cell mui-col-xs-10">'+
+                        '<h4 class="mui-ellipsis">信息化推进办公室张彦合同付款信息化</h4>'+
+                        '<h5>申请人：李四</h5>'+
+                        '<p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>'+
+                      '</div>'+
+                      '<div class="mui-table-cell mui-col-xs-2 mui-text-right">'+
+                        '<span class="mui-h5">12:25</span>'+
+                      '</div>'+
+                    '</div>'+
+                  '</li>';
+            })
+            $('#ul_content').html(str);
+          }else{
+            this.$Message.error("无项目信息");
+          }
       })
     }
-    // getProject() {
-    //   // this.tableLoading = true;
-    //   // this.searchForm.is_audit = this.$route.params.is_audit;
-    //   getAllProjects({userid:sessionStorage.getItem('user_id')}).then(e => {
-    //     this.data = e.result;
-    //     // //分页显示所有数据总数
-    //     // this.dataCount = this.data.length;
-    //     // //循环展示页面刚加载时需要的数据条数
-    //     // this.nowData = [];
-    //     // for (let i = 0; i < this.pageSize; i++) {
-    //     //   if (this.data[i]) {
-    //     //     this.nowData.push(this.data[i]);
-    //     //   }
-    //     // }
-    //     // this.pageCurrent = 1;
-
-    //     // if (this.searchForm.title || this.searchForm.subject || this.searchForm.office || this.searchForm.unit || this.searchForm.num || this.searchForm.type || this.searchForm.build_type || this.searchForm.money_from || this.searchForm.is_gc || this.searchForm.nep_type || this.searchForm.status) {
-    //     //   this.exportBtnDisable = false;
-    //     // }
-    //     // this.tableLoading = false;
-    //   });
-    // }
   }
 };
 </script>
