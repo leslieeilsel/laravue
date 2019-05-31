7749b59406721a539f6ac83ed2f67d14742d78a4
wmin_f <1208444024@qq.com>
2019-05-31 12:43:10 +0800

    ding test

--- resources/assets/js/views/ding/project/list.vue
+++ resources/assets/js/views/ding/project/list.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="mui-content">
     <div class="mui-content"> 
 			<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
         {{con_str}}
@@ -39,7 +38,6 @@
         <span class="mui-tab-label">消息</span>
       </a>
     </nav>
-  </div> 
 </template>
 <style scope src="./index.css"></style>
 <style scope src="./mui.css"></style>
@@ -66,19 +64,18 @@ export default {
         return false;
       }
       getAllProjects({userid:sessionStorage.getItem('userid')}).then(e => {
-          alert(JSON.stringify(e))
           if(e.result){
             let str='';
             e.result.forEach(function (row, index) {
               str += '<li class="mui-table-view-cell">'+
                     '<div class="mui-table">'+
                       '<div class="mui-table-cell mui-col-xs-10">'+
-                        '<h4 class="mui-ellipsis">信息化推进办公室张彦合同付款信息化</h4>'+
-                        '<h5>申请人：李四</h5>'+
-                        '<p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>'+
+                        '<h4 class="mui-ellipsis">'+row.title+'</h4>'+
+                        '<h5>'+row.status+'</h5>'+
+                        '<p class="mui-h6 mui-ellipsis">'+row.type+'</p>'+
                       '</div>'+
                       '<div class="mui-table-cell mui-col-xs-2 mui-text-right">'+
-                        '<span class="mui-h5">12:25</span>'+
+                        '<span class="mui-h5">'+row.created_at+'</span>'+
                       '</div>'+
                     '</div>'+
                   '</li>';
