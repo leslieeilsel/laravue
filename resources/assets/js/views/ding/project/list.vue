ed7311d33a6db8a6b7a3e1c36ca27a7d0193fc29
wmin_f <1208444024@qq.com>
2019-05-31 14:19:49 +0800

    ding test

--- resources/assets/js/views/ding/project/list.vue
+++ resources/assets/js/views/ding/project/list.vue
@@ -1,32 +1,8 @@
 <template>
     <div class="mui-content"> 
-      <div class="mui-content"> 
+      <div class="mui-content" style="margin-bottom:30px"> 
         <ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
           {{con_str}}
-          <!-- <li class="mui-table-view-cell">
-            <div class="mui-table">
-              <div class="mui-table-cell mui-col-xs-10">
-                <h4 class="mui-ellipsis">信息化推进办公室张彦合同付款信息化</h4>
-                <h5>申请人：李四</h5>
-                <p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
-              </div>
-              <div class="mui-table-cell mui-col-xs-2 mui-text-right">
-                <span class="mui-h5">12:25</span>
-              </div>
-            </div>
-          </li>
-          <li class="mui-table-view-cell">
-            <div class="mui-table">
-              <div class="mui-table-cell mui-col-xs-10">
-                <h4 class="mui-ellipsis-2">信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款信息化推进办公室张彦合同付款</h4>
-                <h5>申请人：李四</h5>
-                <p class="mui-h6 mui-ellipsis">Hi，李明明，申请交行信息卡，100元等你拿，李明明，申请交行信息卡，100元等你拿，</p>
-              </div>
-              <div class="mui-table-cell mui-col-xs-2 mui-text-right">
-                <span class="mui-h5">12:25</span>
-              </div>
-            </div>
-          </li> -->
         </ul>
       </div>
       <nav class="mui-bar mui-bar-tab">
