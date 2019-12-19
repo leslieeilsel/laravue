d806d42f115104ab64bfa6602f97aec6db446029
wmin_f <1208444024@qq.com>
2019-12-19 11:36:26 +0800

    d

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '1208444024@qq.com'),
+        'name' => env('MAIL_FROM_NAME', '未来很长。'),
     ],
 
     /*
